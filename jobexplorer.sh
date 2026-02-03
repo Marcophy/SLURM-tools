d044b1d8ef717c02242b01db82897cbfb64b1d2a
Marco A. Villena <43510394+Marcophy@users.noreply.github.com>
2026-02-03 16:06:56 +0100

    Update jobexplorer.sh indentation fixed
    Indentation problem generation when code is copied from terminal to VScode.

--- jobexplorer.sh
+++ jobexplorer.sh
@@ -16,31 +16,31 @@ NC='\e[0m'
 
 while :
 do
-	clear
-	echo -e "${PURPLE}#### JOBS EXPLORER ####\nVersion $version\n"
-	echo -e "${YELLOW} OPTIONS"
-	echo "---------"
-	echo -e "1 - Display status of your jobs"
-	echo -e "2 - Check in loop"
-	echo -e "3 - Cancel a job"
-	echo -e "4 - Cancel all jobs"
-	echo -e "5 - Jobs folders"
-	echo -e "6 - Jobs record"
-	echo -e "7 - Job statistic"
-	echo -e "8 - User and jobs (${RED}slow${YELLOW})"
-	echo -e "0 - Exit${NC}"
-	echo " "
-	echo -n "Select one option [1-6,0]: "
-	read option
-	echo " "
+        clear
+        echo -e "${PURPLE}#### JOBS EXPLORER ####\nVersion $version\n"
+        echo -e "${YELLOW} OPTIONS"
+        echo "---------"
+        echo -e "1 - Display status of your jobs"
+        echo -e "2 - Check in loop"
+        echo -e "3 - Cancel a job"
+        echo -e "4 - Cancel all jobs"
+        echo -e "5 - Jobs folders"
+        echo -e "6 - Jobs record"
+        echo -e "7 - Job statistic"
+        echo -e "8 - User and jobs (${RED}slow${YELLOW})"
+        echo -e "0 - Exit${NC}"
+        echo " "
+        echo -n "Select one option [1-6,0]: "
+        read option
+        echo " "
 
-	case $option in
-	1) echo -e "\n${PURPLE}**** LIST OF JOBS STATUS ****${NC}\n"
-	   echo -e "${GREEN}$(squeue -u $USER -r -l -O jobid,name,state,timeused,Account,Reason)${NC}"
-	   echo -e "\nPress ENTER to continue ..."
-	   read foo;;
+        case $option in
+        1) echo -e "\n${PURPLE}**** LIST OF JOBS STATUS ****${NC}\n"
+           echo -e "${GREEN}$(squeue -u $USER -r -l -O jobid,name,state,timeused,Account,Reason)${NC}"
+           echo -e "\nPress ENTER to continue ..."
+           read foo;;
 
-	2) echo -e "\n${PURPLE}**** LOOP SCAN ****${NC}\n"
+        2) echo -e "\n${PURPLE}**** LOOP SCAN ****${NC}\n"
             echo -n "Refresh rate (seconds): "
             read dela
             while true; do
@@ -51,158 +51,159 @@ do
                 sleep $dela
             done
             read foo;;
-	    
-	3) echo -e "\n${PURPLE}**** CANCEL A JOB ****${NC}\n"
-	   echo -e "${GREEN}$(squeue -u $USER -r -l -O jobid,jobarrayid,name,state,timeused)${NC}\n"
-	   echo -n "Write the ID of the job (C-Cancel): "
-	   read jcancel
-	   if [ $jcancel = "C" ]; then
-	       echo -e "\nOPERATION CANCELLED. Press enter to continue.\n"
-	   else
-	       if grep -q "$jcancel" <<< "$(squeue -r -u $USER --jobs $jcancel -l -O jobid,jobarrayid)"; then
-		   echo -e "\nAre you sure you want to cancel the job ${YELLOW}$jcancel${NC}?"
-		   select QCON in "YES" "NO"; do
-	       	       case $QCON in
-			   YES)
-			       scancel $jcancel
-			       echo -e "\nJob ID = $jcancel cancelled successfully"
-			       break;;
-			   NO)
-			       echo -e "\nJob NO cancelled"
-			       break;;
-		       esac
-		   done
-	       else
-		   echo -e "${REDYELLOW}ERROR! The jobID $jcancel does not match any submitted jobs.${NC}\n"
-	       fi
-	   fi
-	   read foo;;
-	
-	4) echo -e "\n${PURPLE}**** CANCEL ALL JOBS ****${NC}\n"
-	   echo "Are you sure you want to cancel all your jobs in squeue?"
-	   select QCON in "Yes" "No"; do
-	       case $QCON in
-		   Yes)
-		       scancel -u $USER
-		       echo "Jobs canceled"
-		       break;;
-		   No)
-		       echo "Jobs NO cancelled"
-		       break;;
-	       esac
-	   done
-	   read foo;;
 
-	5) echo -e "\n${PURPLE}**** JOBS FOLDER ****${NC}\n"
-	   printf "${GREEN}%-10s %-30s %s\n" "JOBID" "NAME" "WORKDIR"
-	   printf "%-10s %-30s %s\n" "-----" "----" "-------"
-	   squeue -u $USER --noheader --format="%.18A %.30j %Z" |
-	       while read -r jobid name workdir; do
-		   printf "%-10s %-30s %s\n" "$jobid" "$name" "$workdir"
-	       done
-	   echo -e "\n${NC}Press ENTER to continue ..."
-	   read foo;;
+        3) echo -e "\n${PURPLE}**** CANCEL A JOB ****${NC}\n"
+           echo -e "${GREEN}$(squeue -u $USER -r -l -O jobid,jobarrayid,name,state,timeused)${NC}\n"
+           echo -n "Write the ID of the job (C-Cancel): "
+           read jcancel
+           if [ $jcancel = "C" ]; then
+               echo -e "\nOPERATION CANCELLED. Press enter to continue.\n"
+           else
+               if grep -q "$jcancel" <<< "$(squeue -r -u $USER --jobs $jcancel -l -O jobid,jobarrayid)"; then
+                   echo -e "\nAre you sure you want to cancel the job ${YELLOW}$jcancel${NC}?"
+                   select QCON in "YES" "NO"; do
+                       case $QCON in
+                           YES)
+                               scancel $jcancel
+                               echo -e "\nJob ID = $jcancel cancelled successfully"
+                               break;;
+                           NO)
+                               echo -e "\nJob NO cancelled"
+                               break;;
+                       esac
+                   done
+               else
+                   echo -e "${REDYELLOW}ERROR! The jobID $jcancel does not match any submitted jobs.${NC}\n"
+               fi
+           fi
+           read foo;;
 
-	6) echo -e "\n${PURPLE}**** JOBS RECORDS ****${NC}\n"
-	   sacct -u $USER
-	   echo -e "\nPress ENTER to continue ..."
-	   read foo;;
+        4) echo -e "\n${PURPLE}**** CANCEL ALL JOBS ****${NC}\n"
+           echo "Are you sure you want to cancel all your jobs in squeue?"
+           select QCON in "Yes" "No"; do
+               case $QCON in
+                   Yes)
+                       scancel -u $USER
+                       echo "Jobs canceled"
+                       break;;
+                   No)
+                       echo "Jobs NO cancelled"
+                       break;;
+               esac
+           done
+           read foo;;
 
-	7) echo -e "\n${PURPLE}**** JOB STATISTIC ****${NC}\n"
-	   echo -e "${GREEN}$(squeue -u $USER -l -O jobid,jobarrayid,name,state,timeused,nodelist)${NC}\n"
-	   echo -n "Write the ID of the job: "
-	   read jstat
-	   echo " "
-	   sstat $jstat
-	   echo -e "\nPress ENTER to continue ..."
-	   read foo;;
+        5) echo -e "\n${PURPLE}**** JOBS FOLDER ****${NC}\n"
+           printf "${GREEN}%-10s %-30s %s\n" "JOBID" "NAME" "WORKDIR"
+           printf "%-10s %-30s %s\n" "-----" "----" "-------"
+           squeue -u $USER --noheader --format="%.18A %.30j %Z" |
+               while read -r jobid name workdir; do
+                   printf "%-10s %-30s %s\n" "$jobid" "$name" "$workdir"
+               done
+           echo -e "\n${NC}Press ENTER to continue ..."
+           read foo;;
 
-	8) echo -e "\n${PURPLE}**** USERS AND THEIR JOBS ****${NC}\n"
-	   # Define the states of interest
-	   STATES_OF_INTEREST=("RUNNING" "PENDING" "COMPLETING")
-	   OTHER_STATES=("BOOT_FAIL" "CANCELLED" "COMPLETED" "CONFIGURING" "DEADLINE" "FAILED" "NODE_FAIL" "OUT_OF_MEMORY" "PREEMPTED" "RESV_DEL_HOLD" "REQUEUE_FED" "REQUEUE_HOLD" "REQUEUED" "RESIZING" "REVOKED" "SIGNALING" "SPECIAL_EXIT" "STAGE_OUT" "STOPPED" "SUSPENDED" "TIMEOUT")
+        6) echo -e "\n${PURPLE}**** JOBS RECORDS ****${NC}\n"
+           sacct -u $USER
+           echo -e "\nPress ENTER to continue ..."
+           read foo;;
 
-	   # Variable for storing counters: user_status -> quantity
-	   declare -A job_counts
+        7) echo -e "\n${PURPLE}**** JOB STATISTIC ****${NC}\n"
+           echo -e "${GREEN}$(squeue -u $USER -l -O jobid,jobarrayid,name,state,timeused,nodelist)${NC}\n"
+           echo -n "Write the ID of the job: "
+           read jstat
+           echo " "
+           sstat $jstat
+           echo -e "\nPress ENTER to continue ..."
+           read foo;;
 
-	   # Variable for storing unique users
-	   declare -A users
+        8) echo -e "\n${PURPLE}**** USERS AND THEIR JOBS ****${NC}\n"
+           # Define the states of interest
+           STATES_OF_INTEREST=("RUNNING" "PENDING" "COMPLETING")
+           OTHER_STATES=("BOOT_FAIL" "CANCELLED" "COMPLETED" "CONFIGURING" "DEADLINE" "FAILED" "NODE_FAIL" "OUT_OF_MEMORY" "PREEMPTED" "RESV_DEL_HOLD" "REQU
+EUE_FED" "REQUEUE_HOLD" "REQUEUED" "RESIZING" "REVOKED" "SIGNALING" "SPECIAL_EXIT" "STAGE_OUT" "STOPPED" "SUSPENDED" "TIMEOUT")
 
-	   # Capture squeue output into variable
-	   squeue_output=$(squeue -o %u,%T 2>/dev/null)
+           # Variable for storing counters: user_status -> quantity
+           declare -A job_counts
 
-	   # Process line by line (skipping headers)
-	   while IFS=',' read -r user state; do
-	       # Skip header line
-	       if [[ "$user" == "USER" ]]; then
-		   continue
-	       fi
+           # Variable for storing unique users
+           declare -A users
 
-	       # Remove blank spaces
-	       user=$(echo "$user" | xargs)
-	       state=$(echo "$state" | xargs)
+           # Capture squeue output into variable
+           squeue_output=$(squeue -o %u,%T 2>/dev/null)
 
-	       # Mark user as read
-	       users["$user"]=1
+           # Process line by line (skipping headers)
+           while IFS=',' read -r user state; do
+               # Skip header line
+               if [[ "$user" == "USER" ]]; then
+                   continue
+               fi
 
-	       # Count in the appropriate category
-	       if [[ " ${STATES_OF_INTEREST[@]} " =~ " ${state} " ]]; then
-		   ((job_counts["${user}_${state}"]++))
-	       else
-		   ((job_counts["${user}_OTHER"]++))
-	       fi
-	   done <<< "$squeue_output"
+               # Remove blank spaces
+               user=$(echo "$user" | xargs)
+               state=$(echo "$state" | xargs)
 
-	   # Sort users
-	   printf '%s\n' "${!users[@]}" | sort > /tmp/users_sorted.tmp
-	   mapfile -t sorted_users < /tmp/users_sorted.tmp
-	   rm /tmp/users_sorted.tmp
+               # Mark user as read
+               users["$user"]=1
 
-	   # Print table header
-	   printf "%-20s %10s %10s %12s %10s\n" "USER" "RUNNING" "PENDING" "COMPLETING" "OTHER"
-	   printf "%s\n" "$(printf '%.0s-' {1..66})"
+               # Count in the appropriate category
+               if [[ " ${STATES_OF_INTEREST[@]} " =~ " ${state} " ]]; then
+                   ((job_counts["${user}_${state}"]++))
+               else
+                   ((job_counts["${user}_OTHER"]++))
+               fi
+           done <<< "$squeue_output"
 
-	   # Print rows
-	   for user in "${sorted_users[@]}"; do
-	       running=${job_counts["${user}_RUNNING"]:-0}
-	       pending=${job_counts["${user}_PENDING"]:-0}
-	       completing=${job_counts["${user}_COMPLETING"]:-0}
-	       other=${job_counts["${user}_OTHER"]:-0}
+           # Sort users
+           printf '%s\n' "${!users[@]}" | sort > /tmp/users_sorted.tmp
+           mapfile -t sorted_users < /tmp/users_sorted.tmp
+           rm /tmp/users_sorted.tmp
 
-	       printf "%-20s %10d %10d %12d %10d\n" "$user" "$running" "$pending" "$completing" "$other"
-	   done
+           # Print table header
+           printf "%-20s %10s %10s %12s %10s\n" "USER" "RUNNING" "PENDING" "COMPLETING" "OTHER"
+           printf "%s\n" "$(printf '%.0s-' {1..66})"
 
-	   # Separation line
-	   printf "%s\n" "$(printf '%.0s-' {1..66})"
+           # Print rows
+           for user in "${sorted_users[@]}"; do
+               running=${job_counts["${user}_RUNNING"]:-0}
+               pending=${job_counts["${user}_PENDING"]:-0}
+               completing=${job_counts["${user}_COMPLETING"]:-0}
+               other=${job_counts["${user}_OTHER"]:-0}
 
-	   # Totals
-	   total_running=0
-	   total_pending=0
-	   total_completing=0
-	   total_other=0
+               printf "%-20s %10d %10d %12d %10d\n" "$user" "$running" "$pending" "$completing" "$other"
+           done
 
-	   for key in "${!job_counts[@]}"; do
-	       if [[ "$key" == *"_RUNNING" ]]; then
-		   ((total_running += job_counts[$key]))
-	       elif [[ "$key" == *"_PENDING" ]]; then
-		   ((total_pending += job_counts[$key]))
-	       elif [[ "$key" == *"_COMPLETING" ]]; then
-		   ((total_completing += job_counts[$key]))
-	       elif [[ "$key" == *"_OTHER" ]]; then
-		   ((total_other += job_counts[$key]))
-	       fi
-	   done
+           # Separation line
+           printf "%s\n" "$(printf '%.0s-' {1..66})"
 
-	   printf "%-20s %10d %10d %12d %10d\n" "TOTAL" "$total_running" "$total_pending" "$total_completing" "$total_other"
+           # Totals
+           total_running=0
+           total_pending=0
+           total_completing=0
+           total_other=0
 
-	   echo -e "\nPress ENTER to continue ..."
-	   read foo;;
+           for key in "${!job_counts[@]}"; do
+               if [[ "$key" == *"_RUNNING" ]]; then
+                   ((total_running += job_counts[$key]))
+               elif [[ "$key" == *"_PENDING" ]]; then
+                   ((total_pending += job_counts[$key]))
+               elif [[ "$key" == *"_COMPLETING" ]]; then
+                   ((total_completing += job_counts[$key]))
+               elif [[ "$key" == *"_OTHER" ]]; then
+                   ((total_other += job_counts[$key]))
+               fi
+           done
 
-	0) echo -e "\nHave a good day!\n"
-	   exit 1;;
+           printf "%-20s %10d %10d %12d %10d\n" "TOTAL" "$total_running" "$total_pending" "$total_completing" "$total_other"
 
-	*) echo " "
-	   echo -e "\n\e[41;93m$option is an invalid option. Please, try again!${NC}"
-	   read foo;;
-	esac
+           echo -e "\nPress ENTER to continue ..."
+           read foo;;
+
+        0) echo -e "\nHave a good day!\n"
+           exit 1;;
+
+        *) echo " "
+           echo -e "\n\e[41;93m$option is an invalid option. Please, try again!${NC}"
+           read foo;;
+        esac
 done
