5b19f79acd4187840b61dac452d7b88a1c0de329
Marco A. Villena <43510394+Marcophy@users.noreply.github.com>
2026-02-03 12:37:35 +0100

    Update jobexplorer.sh v5.0 -> v6.0
    Update option 8 from jobexplorer to show the jobs list as a table separate by job state.

--- jobexplorer.sh
+++ jobexplorer.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # JOBEXPLORER
 # -----------
-
-# Author: Marco A. Villena (@)
-# Date: 2020 - 2024
-
+# Author: Marco A. Villena (mavillena@ugr.es)
+# Date: 2020 - 2026
 
 version="6.0"
 
@@ -21,7 +19,7 @@ do
 	echo -e "5 - Jobs folders"
 	echo -e "6 - Jobs record"
 	echo -e "7 - Job statistic"
-	echo -e "8 - Users and jobs"
+	echo -e "8 - Users and jobs (slow)"
 	echo -e "0 - Exit\e[0m"
 	echo " "
 	echo -n "Select one option [1-6,0]: "
@@ -30,7 +28,7 @@ do
 
 	case $option in
 	1) echo -e "\n\e[0;95m**** LIST OF JOBS STATUS ****\e[0m\n"
-	   echo -e "\e[0;92m$(squeue -u $USER -r -l -O jobid,name,state,timeused,EndTime,Account)\e[0m"
+	   echo -e "\e[0;92m$(squeue -u $USER -r -l -O jobid,name,state,timeused,Account,Reason)\e[0m"
 	   read foo;;
 
 	2) echo -e "\n\e[0;95m**** LOOP SCAN ****\e[0m\n"
@@ -103,21 +101,83 @@ do
 	   read foo;;
 
 	8) echo -e "\n\e[0;95m**** USERS AND THEIR JOBS ****\e[0m\n"
-	   name_list=$(squeue -O UserName)
-	   name_list="${name_list#* }"
+	   # Define the states of interest
+	   STATES_OF_INTEREST=("RUNNING" "PENDING" "COMPLETING")
+	   OTHER_STATES=("BOOT_FAIL" "CANCELLED" "COMPLETED" "CONFIGURING" "DEADLINE" "FAILED" "NODE_FAIL" "OUT_OF_MEMORY" "PREEMPTED" "RESV_DEL_HOLD" "REQUEUE_FED" "REQUEUE_HOLD" "REQUEUED" "RESIZING" "REVOKED" "SIGNALING" "SPECIAL_EXIT" "STAGE_OUT" "STOPPED" "SUSPENDED" "TIMEOUT")
 
-	   declare -A counter
+	   # Variable for storing counters: user_status -> quantity
+	   declare -A job_counts
 
-	   for name in $name_list; do
-	       ((counter[$name]++))
+	   # Variable for storing unique users
+	   declare -A users
+
+	   # Capture squeue output into variable
+	   squeue_output=$(squeue -o %u,%T 2>/dev/null)
+
+	   # Process line by line (skipping headers)
+	   while IFS=',' read -r user state; do
+	       # Skip header line
+	       if [[ "$user" == "USER" ]]; then
+		   continue
+	       fi
+
+	       # Remove blank spaces
+	       user=$(echo "$user" | xargs)
+	       state=$(echo "$state" | xargs)
+
+	       # Mark user as read
+	       users["$user"]=1
+
+	       # Count in the appropriate category
+	       if [[ " ${STATES_OF_INTEREST[@]} " =~ " ${state} " ]]; then
+		   ((job_counts["${user}_${state}"]++))
+	       else
+		   ((job_counts["${user}_OTHER"]++))
+	       fi
+	   done <<< "$squeue_output"
+
+	   # Sort users
+	   printf '%s\n' "${!users[@]}" | sort > /tmp/users_sorted.tmp
+	   mapfile -t sorted_users < /tmp/users_sorted.tmp
+	   rm /tmp/users_sorted.tmp
+
+	   # Print table header
+	   printf "%-20s %10s %10s %12s %10s\n" "USER" "RUNNING" "PENDING" "COMPLETING" "OTHER"
+	   printf "%s\n" "$(printf '%.0s-' {1..66})"
+
+	   # Print rows
+	   for user in "${sorted_users[@]}"; do
+	       running=${job_counts["${user}_RUNNING"]:-0}
+	       pending=${job_counts["${user}_PENDING"]:-0}
+	       completing=${job_counts["${user}_COMPLETING"]:-0}
+	       other=${job_counts["${user}_OTHER"]:-0}
+
+	       printf "%-20s %10d %10d %12d %10d\n" "$user" "$running" "$pending" "$completing" "$other"
 	   done
 
-	   echo -e "\e[0;92mUSER: Submitted jobs"
-	   echo -e "--------------------\e[0m"
-	   for name in "${!counter[@]}"; do
-	       echo -e "$name: ${counter[$name]}"
+	   # Separation line
+	   printf "%s\n" "$(printf '%.0s-' {1..66})"
+
+	   # Totals
+	   total_running=0
+	   total_pending=0
+	   total_completing=0
+	   total_other=0
+
+	   for key in "${!job_counts[@]}"; do
+	       if [[ "$key" == *"_RUNNING" ]]; then
+		   ((total_running += job_counts[$key]))
+	       elif [[ "$key" == *"_PENDING" ]]; then
+		   ((total_pending += job_counts[$key]))
+	       elif [[ "$key" == *"_COMPLETING" ]]; then
+		   ((total_completing += job_counts[$key]))
+	       elif [[ "$key" == *"_OTHER" ]]; then
+		   ((total_other += job_counts[$key]))
+	       fi
 	   done
-           read foo;;
+
+	   printf "%-20s %10d %10d %12d %10d\n" "TOTAL" "$total_running" "$total_pending" "$total_completing" "$total_other"
+	   read foo;;
 
 	0) echo -e "\nHave a good day!\n"
 	   exit 1;;
